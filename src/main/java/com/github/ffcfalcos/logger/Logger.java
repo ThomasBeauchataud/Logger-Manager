00beb9b2dfb6d56a3c23e13833c1249cd4fad785
ffcfalcos <thomas.beauchataud@yahoo.fr>
2019-11-02 20:24:29 -0400

    init commit

--- src/main/java/com/github/ffcfalcos/logger/Logger.java
+++ src/main/java/com/github/ffcfalcos/logger/Logger.java
@@ -3,18 +3,49 @@ package com.github.ffcfalcos.logger;
 import com.github.ffcfalcos.rabbitmq.RabbitMQManager;
 import com.github.ffcfalcos.rabbitmq.RabbitMQManagerInterface;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 
+/**
+ * At the construction of the logger, we try to find env-entry parameters
+ * If they are set, all parameters are automatically load
+ * Parameters for Rabbit:
+ *                  rabbitMQ-host
+ *                  rabbitMQ-user
+ *                  rabbitMQ-password
+ *                  rabbitMQ-exchange-logger
+ *                  rabbitMQ-routingKey-logger
+ * Parameters for File:
+ *                  log-path
+ */
 public class Logger implements LoggerInterface {
 
     private final RabbitMQManagerInterface rabbitMQManager = new RabbitMQManager();
     private String filePath;
     private List<String> rabbitMQParameters;
     private int defaultLogger = 0;
+
+    public Logger() {
+        try {
+            Context env = (Context) new InitialContext().lookup("java:comp/env");
+            rabbitMQParameters = Arrays.asList(
+                    (String) env.lookup("rabbitMQ-host"),
+                    (String) env.lookup("rabbitMQ-user"),
+                    (String) env.lookup("rabbitMQ-password"),
+                    (String) env.lookup("rabbitMQ-exchange-logger"),
+                    (String) env.lookup("rabbitMQ-routingKey-logger"));
+            this.filePath = (String) env.lookup("log-path");
+            this.defaultLogger = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void log(String content) {
